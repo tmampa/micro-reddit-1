e337d2a4a97427b67094a5b0e24764fd995743db
tmampa <tmampa94@gmail.com>
2021-06-21 11:18:20 +0200

    Authorization on links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,5 +1,6 @@
 class LinksController < ApplicationController
   before_action :set_link, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /links or /links.json
   def index
