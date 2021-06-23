7b91d3f7d78161ed42e9c9fd64b5896edfa0b1f4
prolajumokeoni <prolajumokeoni@gmail.com>
2021-06-23 17:30:43 +0100

    Update users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,18 @@ class User < ApplicationRecord
   has_many :links
   has_many :comments
 
-  validates :email, presence: true
-  validates :name, presence: true, length: { in: 3..16 }
+  validates :username, presence: true
+
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
+  
+  # use this instead of email_changed? for Rails = 5.1.x
+  def will_save_change_to_email?
+    false
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_22_165727) do
+ActiveRecord::Schema.define(version: 2021_06_23_160500) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "link_id"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2021_06_22_165727) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
