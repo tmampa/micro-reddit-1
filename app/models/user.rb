802c3d6a5ff4845565cf59c16fa2cb12fe4f6d7d
tmampa <tmampa94@gmail.com>
2021-06-23 10:15:34 +0200

    Associate users with the links

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :links
+  has_many :comments
+
+  validates :name, presence: true, length: { in: 3..16 }
 end
